e2014fee563eecef7e19f520f8b5be8a229fc859
WhatsARanjit <whatsaranjit@gmail.com>
2020-06-03 15:57:29 -0400

    Flag debug; optional Consul lookup

--- scripts/batch_splitter.sh
+++ scripts/batch_splitter.sh
@@ -1,13 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
 
-set -e
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+# Debug options - set $BATCH_DEMO_DEBUG to anything
+if [ ! -z "$BATCH_DEMO_DEBUG" ]; then
+  set -x
+  set -e
+  trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+  trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+  CURL_VERBOSE="-v"
+fi
 
 # Set indexes
-START_INDEX=$(($1))
+START_INDEX=$1
 SIZE=$2
-START=$(($START_INDEX*$SIZE+1))
+START=$(expr $START_INDEX "*" $SIZE + 1)
 END=$(($START+$SIZE))
 
 # Collect numbers
@@ -29,15 +34,22 @@ for num in $COLLECT; do
 done
 
 # Slow for demo
-sleep $3
+sleep=${3:-1}
+sleep $sleep
 
 # Trim and fixup payload
 RESULT=${RESULT%??}
 RESULT=$(echo -e $RESULT)
 
 # Lookup Collector address
-COLLECTOR_ADDR=$(dig @localhost -p8600 +short collector.service.consul)
+# Use ENV or check Consul
+if [ -z "$COLLECTOR_ADDR" ]; then
+  COLLECTOR_ADDR=$(dig @localhost -p8600 +short collector.service.consul)
+fi
 echo "Collector address: ${COLLECTOR_ADDR}"
 
 # Submit payload
-curl -v -d "$RESULT" "${COLLECTOR_ADDR}:4567/api"
+curl $CURL_VERBOSE -d "$RESULT" "${COLLECTOR_ADDR}:4567/api"
+
+# Report payload
+echo $RESULT
